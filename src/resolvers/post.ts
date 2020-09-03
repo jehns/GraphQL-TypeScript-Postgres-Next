d82a963260570a2f6f41e5fcef6718b4573f5722
Stuart Jehn <stuartjehn@gmail.com>
2020-09-02 22:19:15 -0700

    crud operations added to graphql schema

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,9 +1,9 @@
 import { MyContext } from '../types';
-import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 import { Post } from "../entities/Post";
 
-// typescript type and type-graphql type must both be set.
 
+// typescript type and type-graphql type must both be set.
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
@@ -19,4 +19,45 @@ export class PostResolver {
     return ctx.em.findOne(Post, { id });
   }
 
+  // to create with a nullable field => { nullable: true }
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title', () => String) title: string,
+    @Ctx() ctx: MyContext
+    ): Promise<Post> {
+      const post = ctx.em.create(Post, {title})
+      await ctx.em.persistAndFlush(post)
+      return post
+  }
+
+  @Mutation(() => Post, {nullable: true})
+  async updatePost(
+    @Arg('id', () => Int) id: number,
+    @Arg("title", () => String, {nullable: true}) title: string,
+    @Ctx() ctx: MyContext
+    ): Promise<Post | null> {
+      const post = await ctx.em.findOne(Post, {id})
+      if (!post) {
+        return null
+      }
+      if (typeof title !== 'undefined') {
+        post.title = title
+        await ctx.em.persistAndFlush(post)
+      }
+      return post
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg('id', () => Int) id: number,
+    @Ctx() ctx: MyContext
+    ): Promise<boolean> {
+      try {
+        // need to refactor - does not error if post does not exist
+        await ctx.em.nativeDelete(Post, {id})
+        return true
+      } catch (error) {
+        return false
+      }
+  }
 }
